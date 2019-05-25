4dbfda7866164b00b0e107f27fa70e499d2b9bd4
HarutoHiroki <hiroaki.haruto@gmail.com>
2019-05-25 14:46:42 +0700

    /shrug

--- commands/help.js
+++ commands/help.js
@@ -5,34 +5,13 @@ const settings = require('../settings.json');
 const fs = require('fs')
 exports.run = (client, message, args) => {
   if(!args[0]){
-      if (message.author.id === settings.ownerid) {  
-        const embed = new Discord.RichEmbed()
-        .addField("All commands have been migrated to here:", "https://harutohiroki.github.io/commands.html")
-        .addField("For more info about a specific Command:", "Use [prefix]help command_name")
-        .addField('Bot Owner Commands', help.helpMsg4)
-        .setFooter(`© Cryptonix X Mod Bot by ${customisation.ownername}`);
-        message.author.send({embed}).catch(e =>{
-          if (e) {
-          message.channel.send(`Error. You seems to be locking your DMs so I'll send it here instead.`);
-          message.channel.send({embed});
-          }
-        });
-        message.reply("Check your DMs!");
-      } else {
-        const embed = new Discord.RichEmbed()
-        .setColor(Math.floor(Math.random()*16777215))
-        .setTitle("Command list for Cryptonix:", '')
-        .addField("All commands have been migrated to here:", "https://harutohiroki.github.io/commands.html")
-        .addField("For more info about a specific Command:", "Use [prefix]help command_name")
-        .setFooter(`© Cryptonix X Mod Bot by ${customisation.ownername}`);
-        message.author.send({embed}).catch(e =>{
-          if (e) {
-          message.channel.send(`Error. You seems to be locking your DMs so I'll send it here instead.`);
-          message.channel.send({embed});
-          }
-        });
-        message.reply("Check your DMs!");
+      message.author.send("```" + "***Moderation***\n" + `${help.helpMsg1}` + "```" + "\n" + "```" + "***Usefull***\n" + `${help.helpMsg2}` + "```" + "\n" + "```"+"***Fun***\n"+`${help.helpMsg3}`+"```" + "```" + "```"+"***Action***\n"+`${help.helpMsg4}`+"```")
+      .catch(e =>{
+        if (e) {
+        message.channel.send(`Error. You seems to be locking your DMs so I'll send it here instead.`);
+        message.author.send("```" + "***Moderation***\n" + `${help.helpMsg1}` + "```" + "\n" + "```" + "***Usefull***\n" + `${help.helpMsg2}` + "```" + "\n" + "```"+"***Fun***\n"+`${help.helpMsg3}`+"```" + "```" + "```"+"***Action***\n"+`${help.helpMsg4}`+"```")
       }
+      });
     }else{
       let command = args[0];
       if (client.commands.has(command)) {
