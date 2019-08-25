e12ee4ed4b2dd5fa952c428a196fdb722317e78d
Soheab Malik <soheabdebeste@hotmail.com>
2019-08-25 20:46:59 +0200

    fix for error: DeprecationWarning
    Fixed warning appearing when doing a command
    error: Collection#find: pass a function instead. no sure if this is how it works 🤷‍♂ 
    Source from where i got it: https://stackoverflow.com/questions/52026292/deprecationwarning-collectionfind-pass-a-function-instead

--- index.js
+++ index.js
@@ -79,18 +79,18 @@ client.reload = command => {
 };
 
 client.elevation = message => {
-  if (message.channel.type === 'dm') return;
-  let permlvl = 0;
-  let mod_role = message.guild.roles.find('name', settings.modrolename);
-  if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 1;
-  let admin_role = message.guild.roles.find('name', settings.adminrolename);
-  if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 2;
-  let manager_role = message.guild.roles.find('name', settings.managerrolename);
-  if (manager_role && message.member.roles.has(manager_role.id)) permlvl = 3;
-  let overlord_role = message.guild.roles.find('name', settings.overlordrolename)
-  if (overlord_role && message.member.roles.has(overlord_role.id)) permlvl = 4;
-  if (message.author.id === settings.ownerid) permlvl = 5;
-  return permlvl;
+	if (message.channel.type === 'dm') return;
+	let permlvl = 0;
+	let mod_role = message.guild.roles.find(settings => settings.modrolename === "name");
+	if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 1;
+	let admin_role = message.guild.roles.find(settings => settings.adminrolename === "name");
+	if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 2;
+	let manager_role = message.guild.roles.find(settings => settings.managerrolename === "name");
+	if (manager_role && message.member.roles.has(manager_role.id)) permlvl = 3;
+	let overlord_role = message.guild.roles.find(settings => settings.overlordrolename === "name")
+	if (overlord_role && message.member.roles.has(overlord_role.id)) permlvl = 4;
+	if (message.author.id === settings.ownerid) permlvl = 5;
+	return permlvl;
 };
 
 //ping log 
