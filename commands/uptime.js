a4a6fbd2d3317fff0b13c06a385799bd0ff6d84c
harutohiroki <hiroaki.haruto@gmail.com>
2020-08-05 16:20:10 +0700

    detailed uptime

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,25 +1,27 @@
 exports.run = (client, message) => {
-    var milliseconds = parseInt((client.uptime % 1000) / 100),
-        seconds = parseInt((client.uptime / 1000) % 60),
-        minutes = parseInt((client.uptime / (1000 * 60)) % 60),
-        hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
+  var milliseconds = parseInt((client.uptime % 1000) / 100),
+      seconds = parseInt((client.uptime / 1000) % 60),
+      minutes = parseInt((client.uptime / (1000 * 60)) % 60),
+      hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
+      days = parseInt((client.uptime / (1000 * 60 * 60 * 24)) % 60);
 
-        hours = (hours < 10) ? "0" + hours : hours;
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
+      days = (days < 10) ? "0" + days : days;
+      hours = (hours < 10) ? "0" + hours : hours;
+      minutes = (minutes < 10) ? "0" + minutes : minutes;
+      seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-        message.channel.send(":chart_with_upwards_trend: I've been running for** " + hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!");
+      message.channel.send(":chart_with_upwards_trend: I've been running for** " + days + " **days, **" + hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!");
 }
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
+enabled: true,
+guildOnly: false,
+aliases: [],
+permLevel: 0
 };
 
 exports.help = {
-  name: 'uptime',
-  description: 'Shows how long the bot has been online for.',
-  usage: 'uptime'
+name: 'uptime',
+description: 'Shows how long the bot has been online for.',
+usage: 'uptime'
 };
