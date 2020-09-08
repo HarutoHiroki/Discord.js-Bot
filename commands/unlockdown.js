57b29b540ef0c206e7aaf82acc9e088dadfe389d
harutohiroki <hiroaki.haruto@gmail.com>
2020-09-08 23:49:06 +0700

    version 13 update

--- commands/unlockdown.js
+++ commands/unlockdown.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   if (!client.lockit) client.lockit = [];
   //if (!message.member.hasPermission("MANAGE_CHANNELS")) return msg.reply("❌**Error:** You don't have the permission to do that!");
 
-    message.channel.overwritePermissions(message.guild.id, {
+    message.channel.createOverwrite(message.guild.id, {
       SEND_MESSAGES: null
     }).then(() => {
       message.channel.send('Lockdown lifted <a:balancecheck:556017659419033653> WEEEEEEEEEEEEEEEEEEEEEE, enjoy talking while you still can:wink:');
