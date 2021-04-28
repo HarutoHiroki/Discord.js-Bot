d3d00a0231880d84af90c7d7cc5b0a1ea80d580b
Xynnix <zkjcksn@gmail.com>
2021-04-28 06:13:59 +0000

    Removed unblacklist.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userID: String,
-    hasVoted: Boolean,
     blacklist: Number,
 });
 module.exports = mongoose.model("User",userSchema)
